62e8f50a59215d62e1a8d370cd61e6b0625333fa
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-01-04 03:51:18 -0600

    Add Avalonia project.

---
+++ Concursus.Desktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Concursus.Desktop.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ Concursus.Desktop/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Concursus.Desktop.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
