780fe0b407e1a9a168820a5e4325a844034dca83
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-01-04 19:03:31 -0600

    Add converter for wrapping view models in a ContentPresenter.

---
+++ Concursus.Desktop/Converters/InContentPresenter.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia.Controls.Presenters;
+using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace Concursus.Desktop.Converters;
+
+public class InContentPresenter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => new ContentPresenter() { Content = value };
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is ContentPresenter contentPresenter)
+        {
+            return contentPresenter.Content;
+        }
+
+        return null;
+    }
+}
