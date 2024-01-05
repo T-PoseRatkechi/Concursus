db4f41ce8f94ea936f54de789b67b0f45af78389
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-01-04 19:50:24 -0600

    Implement initial Main Window VM.

--- Concursus.Desktop/ViewModels/MainWindowViewModel.cs
+++ Concursus.Desktop/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,24 @@
-﻿namespace Concursus.Desktop.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Concursus.Library.Games;
+
+namespace Concursus.Desktop.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    [ObservableProperty]
+    private Game currentGame = new("p5t")
+    {
+        Name = "Persona 5 Tactica",
+    };
+
+    public MainWindowViewModel()
+    {
+        
+    }
+
+    public ViewModelBase Home { get; set; } = new ModsViewModel();
+
+    public ViewModelBase Mods { get; set; } = new ModsViewModel();
+
+    public ViewModelBase Settings { get; set; } = new SettingsViewModel();
 }
