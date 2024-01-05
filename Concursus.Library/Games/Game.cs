01f0faeff6104359bb2306eab996f3ee70685158
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-01-04 19:48:14 -0600

    Add game type.

---
+++ Concursus.Library/Games/Game.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Concursus.Library.Games;
+
+public partial class Game : ObservableObject
+{
+    [ObservableProperty]
+    private string? name = string.Empty;
+
+    public Game(string internalName)
+    {
+        this.InternalName = internalName;
+    }
+
+    public string InternalName { get; }
+}
