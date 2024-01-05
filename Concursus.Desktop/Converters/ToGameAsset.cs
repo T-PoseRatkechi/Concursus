99734a4d21c1a781988870f241a3df99db49ab38
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-01-04 19:49:55 -0600

    Add converter to get app assets by game.

---
+++ Concursus.Desktop/Converters/ToGameAsset.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Data;
+using Avalonia.Data.Converters;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using Concursus.Library.Games;
+using System;
+using System.Globalization;
+
+namespace Concursus.Desktop.Converters;
+
+public class ToGameAsset : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is Game game && parameter is string file)
+        {
+            return new Bitmap(AssetLoader.Open(new Uri($"avares://Concursus.Desktop/Assets/Games/{game.InternalName}/{file}")));
+        }
+
+        return value;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return new BindingNotification(new Exception(), BindingErrorType.Error);
+    }
+}
